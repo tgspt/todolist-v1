362db6e2857e743b8c3d50fdc92d3e90b85d029a
tgspt <telmogosilva@gmail.com>
2023-07-14 12:06:17 +0100

    creationg of date.js and exports manage

--- app.js
+++ app.js
@@ -1,33 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require (__dirname + "/date.js");
+
 
 
 const app = express();
 
-let items = ["1thing", "2thing", "3thing"]
-let workItems=[]
+const items = ["1thing", "2thing", "3thing"]
+const workItems=[]
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 app.get("/", function (req, res) {
-    let today = new Date()
-
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-    let day = today.toLocaleDateString("pt-PT", options)
-
-
-
+const day = date.getDate();
 
     res.render("list", { listTitle: day, newListItems: items});
 });
 
 app.post("/", function (req, res) {
-    let item = req.body.newItem;
+    const item = req.body.newItem;
     if(req.body.list==="Work"){
         workItems.push(item);
         res.redirect("/work");
