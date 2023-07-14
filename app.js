fbc525ab0b5cb518bd2e7deb1343108b4c23eb40
tgspt <telmogosilva@gmail.com>
2023-07-14 10:58:41 +0100

    todo list

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+
+const app = express();
+
+let items = ["1thing", "2thing", "3thing"]
+let workItems=[]
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+app.get("/", function (req, res) {
+    let today = new Date()
+
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+    let day = today.toLocaleDateString("pt-PT", options)
+
+
+
+
+    res.render("list", { listTitle: day, newListItems: items});
+});
+
+app.post("/", function (req, res) {
+    let item = req.body.newItem;
+    if(req.body.list==="Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    }   else{
+        items.push(item);
+        res.redirect("/");
+    }
+    
+    
+});
+
+app.get("/work", function(req, res){
+    res.render("list",{listTitle: "Work List", newListItems: workItems });
+});
+
+app.post("/work", function(req, res){
+    let item= req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+});
+
+
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
+});
+
